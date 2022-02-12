88228f03c85ecfb11128433a7d1fcb4c33b13d0e
sannzay <sannzayreddy@gmail.com>
2022-02-13 00:37:54 +0530

    Index and redux commands added

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import moduleNfirebaseame from 'firebase/app'
+import Cosnants from 'expo-constants'
+
+if(firebase.apps.length == 0){
+  firebase.initializeApp(Constants.manifest.web.config.firebase)
+}
+
 
 export default function App() {
   return (
